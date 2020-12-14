20b9341d09c3bcdc20c3e4a15e0ee5e5c868e90f
Zhou Hongyang <hongyang@lx0-500-23094.ad.helsinki.fi>
2020-12-14 16:51:50 +0200

    first commit

---
+++ src/UnitfulBatsrus.jl
@@ -0,0 +1,41 @@
+__precompile__(true)
+module UnitfulBatsrus
+
+import Unitful
+import Unitful: q, c, μ0, ϵ0, k, me, mp
+using Unitful: @unit
+export @bu_str
+
+
+# lengths
+@unit R_bu  "Rₑ"     Radii             (6378)*Unitful.km        false
+
+# masses
+@unit me_bu "mₑ"     ElectronMass      me                       false
+@unit mp_bu "mₚ"     ProtonMass        mp                       false
+@unit u_bu  "u"      AMU               1*Unitful.u              false
+
+# charges
+@unit q_bu  "q"      ElementaryCharge  q                        false
+@unit k_bu  "k"      BoltzmannConstant k                        false
+
+# densities
+@unit amucc_bu "amu/cc" AtomicDensity  1*Unitful.u/Unitful.cm^3 false
+
+# velocities
+@unit V_bu  "V"      Speed             1*Unitful.km/Unitful.s   false
+
+# currents
+@unit J_bu  "J"      Current           1*Unitful.μA/Unitful.m^2 false
+
+include("batsrusmacro.jl")
+
+# Some gymnastics required here because if we precompile, we cannot add to
+# Unitful.basefactors at compile time and expect the changes to persist to runtime.
+const localunits = Unitful.basefactors
+function __init__()
+    merge!(Unitful.basefactors, localunits)
+    Unitful.register(UnitfulBatsrus)
+end
+
+end # module
